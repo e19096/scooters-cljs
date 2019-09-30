45ace9308e223c0ee2b8695789a08be14136eebd
Esther <esther@ip-10-23-16-236.us-east-2.compute.internal>
2019-09-30 13:09:17 -0400

    more things

--- src/scooter_site/core.cljs
+++ src/scooter_site/core.cljs
@@ -1,24 +1,34 @@
 (ns scooter-site.core
     (:require
       [reagent.core :as r]
-      [scooter-site.data :as data]))
+      [scooter-site.data :refer [products]]))
+      ; [scooter-site.data :as data]))
+
+(def cart-count (r/atom 0))
 
 (defn header []
   [:div {:class "Header-container"}
         [:span {:class "Header-title"} "E-scooter Emporium"]
         [:img {:src "/assets/logo.png" :class "Header-logo" :alt "logo"}]
-        0 [:i {:class "material-icons"} "shopping_cart"]])
+        @cart-count [:i {:class "material-icons"} "shopping_cart"]])
 
-(defn product-index
-  ([] [:div [:span "hey, i'm the product index"]])
-  ([something] [:marquee something]))
+(defn product-item [scooter]
+  (let [{:keys [name price image] :as thing} scooter]
+    [:div {:class "prod-item"}
+      [:img {:src image}]
+      [:div.details
+        [:div name]
+        [:div (get-in thing [:price :currency-symbol])
+              (-> scooter :price :value)]
+              [:button {:on-click #(swap! cart-count inc)}
+              "Add"]]]))
+
+(defn product-index []
+  [:div {:class "prod-idx"} (map product-item products)])
 
 (defn home-page []
   [:div [header]
-        [product-index]
-        [product-index "yo yo yo"]
-        [product-index]
-        ])
+        [product-index]])
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
